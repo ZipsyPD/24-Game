514d8b4aaf4fe8de1b6c8fa6a7a587b4ba714281
ZipsyPD <oy7446@princeton.edu>
2026-05-12 02:42:32 +0800

    Test Client for Linux System testing

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,37 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-public class Client 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+
+import java.rmi.Naming;
+
+public class TestClient {
+    public static void main(String[] args) {
+        try {
+            Service service = (Service) Naming.lookup("rmi://localhost/Service");
+
+            System.out.println("Connected to RMI service!");
+
+            System.out.println("Register: " + service.register("bob", "123"));
+            System.out.println("Logout: " + service.logout("bob"));
+            System.out.println("Login: " + service.login("bob", "123"));
+            System.out.println("Logout: " + service.logout("bob"));
+
+        } catch (Exception e) {
+            System.out.println("Client error: " + e);
+            e.printStackTrace();
+        }
+    }
+}
